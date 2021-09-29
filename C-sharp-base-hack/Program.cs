fb18a4af7c3989e72361f2848fd6206d01717888
bonni <bonni@DESKTOP-SASFCGR>
2021-09-28 23:46:13 -0400

    Add project files.

---
+++ C-sharp-base-hack/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp3
+{
+    using C_sharp_base_hack;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process proc = Process.GetProcessesByName("")[0];
+
+            var hProc = api.OpenProcess(api.ProcessAccessFlags.All, false, proc.Id);
+
+            var modBase = api.GetModuleBaseAddress(proc, "exe");
+
+            var modBase2 = api.GetModuleBaseAddress(proc.Id, "exe");
+
+            System.Console.WriteLine("Last Error: " + Marshal.GetLastWin32Error());
+
+
+            Console.ReadKey();
+        }
+    }
+}
